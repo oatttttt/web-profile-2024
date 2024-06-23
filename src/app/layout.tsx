f83c6a7623741dee9672787b2a1cca29e2058080
oatttttt <oatthiraphat@gmail.com>
2024-06-23 11:05:13 +0700

    chaged

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ config.autoAddCss = false;
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Thiraphat Phetsri",
+  title: "Thiraphat Phetsrii",
   description: "personal website",
 };
 
